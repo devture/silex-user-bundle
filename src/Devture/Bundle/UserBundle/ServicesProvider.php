46286a0f0273eefbf4b268237085e4869e8462eb
Slavi Pantaleev <s.pantaleev@gmail.com>
2012-06-24 16:39:07 +0300

    Faster Twig path registration

--- src/Devture/Bundle/UserBundle/ServicesProvider.php
+++ src/Devture/Bundle/UserBundle/ServicesProvider.php
@@ -62,7 +62,7 @@ class ServicesProvider implements ServiceProviderInterface {
 
         $app->before($app['user.access_control']->getEnforcer());
 
-        $app['twig.loader']->addLoader(new \Twig_Loader_Filesystem(dirname(__FILE__) . '/Resources/views/'));
+        $app['twig.loader.filesystem']->addPath(dirname(__FILE__) . '/Resources/views/');
         $app['twig']->addExtension(new \Devture\Bundle\UserBundle\Twig\Extension\UserExtension($app));
         $app['twig']->addExtension(new \Devture\Bundle\UserBundle\Twig\Extension\AccessControlExtension($app['user.access_control']));
     }
